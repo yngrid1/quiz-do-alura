1844436e2a06a08af9511f8f9b4356c051d82549
veia yngrid <00001090517932sp@al.educacao.sp.gov.br>
2024-10-12 11:05:09 -0300

    quiz sobre filmes projeto alura

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filme 1',
+    'Quem é o diretor de "Titanic"?',
+    'R: James Cameron.'
+)
+
+criaCartao(
+    'filme 2',
+    'Em "Matrix", o que representa a pílula vermelha?',
+    'R: A verdade e a realidade.'
+)
+
+criaCartao(
+    'filme 3',
+    'Qual é o famoso bordão de "O Poderoso Chefão"?',
+    'R: Vou fazer uma oferta que ele não poderá recusar'
+)
+
+criaCartao(
+    'filme 4',
+    'Em "Jurassic Park", o que traz os dinossauros de volta à vida?',
+    'R: DNA extraído de mosquitos fossilizados.'
+)
